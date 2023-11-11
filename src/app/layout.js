ac49979c65802f94b7356183453fca12d4fde34c
Abu Osama <abuosamasports@gmail.com>
2023-11-11 23:35:55 +0530

    redux error solved

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,10 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-       <link
-    rel="stylesheet"
-    href=" https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-  />
+     
       <body className={inter.className}>
 
 <Provider store={store}>
